be14a4449e048a2bb0729e89eefbecc29412eb8a
Paulo G Portugal Neto <portugneto@gmail.com>
2021-06-10 00:22:41 -0300

    2.0

---
+++ Assessment-MVC/Assessment-MVC/Controllers/AmigoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment_MVC.Controllers
+{
+    public class AmigoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assessment-MVC/Assessment-MVC/Models/Amigo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment_MVC.Models
+{
+    public class Amigo
+    {
+        public int AmigoId { get; set; }
+        public string Nome { get; set; }
+        public DateTime Aniversario { get; set; }
+    }
+}
